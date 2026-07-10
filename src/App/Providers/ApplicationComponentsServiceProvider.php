728326e1f6e710df57fe579d1b40934198cd9ef7
oleh <OlegGin@gmai.com>
2026-07-10 11:41:23 +0300

    Added component registrars to ApplicationComponentsServiceProvider

--- src/App/Providers/ApplicationComponentsServiceProvider.php
+++ src/App/Providers/ApplicationComponentsServiceProvider.php
@@ -4,11 +4,19 @@ namespace Concept\App\Providers;
 
 use Concept\App\Foundation\ConfigKey;
 use Concept\Core\Container\ContainerDependency;
+use Concept\Extensions\Components\Commands\ComponentListCommand;
+use Concept\Extensions\Components\Commands\ComponentPublishAssetsCommand;
+use Concept\Extensions\Components\ComponentRegistry;
 use Concept\Extensions\Components\ComponentsServiceProvider;
 use Concept\Extensions\Components\Contracts\ComponentInterface;
 use Concept\Extensions\Config\Contracts\ConfigInterface;
+use Concept\Extensions\DatabaseEloquent\Registries\MigrationRegistry;
+use Concept\Extensions\DatabaseEloquent\Registries\SeederRegistry;
+use Concept\Extensions\View\Registry\ViewRegistry;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 use League\Container\ServiceProvider\BootableServiceProviderInterface;
+use League\Route\Router;
+use Symfony\Component\Console\Application as ConsoleApplication;
 
 final class ApplicationComponentsServiceProvider extends AbstractServiceProvider implements BootableServiceProviderInterface
 {
@@ -32,6 +40,38 @@ final class ApplicationComponentsServiceProvider extends AbstractServiceProvider
 
         $container->addServiceProvider(new ComponentsServiceProvider(
             componentClasses: $componentClasses,
+            seedersRegistrar: function(ComponentRegistry $registry) use ($container): void {
+                ContainerDependency::get($container, SeederRegistry::class)->append($registry->seeders());
+            },
+            migrationsRegistrar: function(ComponentRegistry $registry) use ($container): void {
+                ContainerDependency::get($container, MigrationRegistry::class)->append($registry->migrationPaths());
+            },
+            commandsRegistrar: function(ComponentRegistry $registry) use ($container): void {
+                $consoleApplication = ContainerDependency::get($container, ConsoleApplication::class);
+
+                $consoleApplication->addCommand(new ComponentListCommand($registry));
+                $consoleApplication->addCommand(new ComponentPublishAssetsCommand($registry));
+
+                foreach ($registry->commands() as $commandClass) {
+                    $command = ContainerDependency::get($container, $commandClass);
+                    $consoleApplication->addCommand($command);
+                }
+            },
+            routesRegistrar: function(ComponentRegistry $registry) use ($container): void {
+                $router = ContainerDependency::get($container, Router::class);
+
+                foreach ($registry->routes() as $routesFile) {
+                    require $routesFile;
+                }
+            },
+            viewFeaturesRegistrar: PHP_SAPI !== 'cli'
+                ? function(ComponentRegistry $registry) use ($container): void {
+                    $viewRegistry = ContainerDependency::get($container, ViewRegistry::class);
+                    $viewRegistry->extensions()->append($registry->viewExtensions());
+                    $viewRegistry->paths()->append($registry->viewPaths());
+                    $viewRegistry->routeNamespace()->append($registry->viewRouteNamespace());
+                }
+                : null,
         ));
     }
 }
